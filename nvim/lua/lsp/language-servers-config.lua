d9b852d0145d9720115261e35aca26ff9e7b0f85
Aldo Sierra <aldo.sierrav@gmail.com>
2021-12-08 20:40:56 -0600

    Fix lang servers nvim

--- nvim/lua/lsp/language-servers-config.lua
+++ nvim/lua/lsp/language-servers-config.lua
@@ -1,33 +1,46 @@
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Emmet Config (needs npm install -g ls_emmet)
-require'lspconfig.configs'.ls_emmet = {
-  default_config = {
-    cmd = {'ls_emmet', '--stdio'},
-    filetypes = {
-      'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml', 'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus',
-      'less', 'sss'
-    },
-    root_dir = function(_)
-      return vim.loop.cwd()
-    end,
-    settings = {}
-  }
-}
-local langservers = {'html', 'cssls', 'tsserver', 'pyright', 'ls_emmet'}
--- Require their respective language server
-for _, server in ipairs(langservers) do require('lspconfig')[server].setup {capabilities = capabilities} end
--- Lua Language Server
 local runtime_path = vim.split(package.path, ';')
+local configs = require'lspconfig.configs'
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {version = 'LuaJIT', path = runtime_path},
-      diagnostics = {globals = {'vim', 'use'}},
-      workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
-      telemetry = {enable = false}
-    }
+if not configs.ls_emmet then
+  configs.ls_emmet = {
+    default_config = {
+      cmd = { 'ls_emmet', '--stdio' };
+      filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml',
+        'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'};
+      root_dir = function(_)
+        return vim.loop.cwd()
+      end;
+      settings = {};
+    };
   }
+end
+-- Set lsp servers var (emmet needs npm install -g ls_emmet)
+local langservers = {
+  'html',
+  'cssls',
+  'tsserver',
+  'pyright',
+  'ls_emmet',
+  'sumneko_lua',
 }
+-- Require their respective language server
+for _, server in pairs(langservers) do
+  if server == 'sumneko_lua' then
+    require'lspconfig'[server].setup {
+      settings = {
+        Lua = {
+          runtime = {version = 'LuaJIT', path = runtime_path},
+          diagnostics = {globals = {'vim', 'use'}},
+          workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
+          telemetry = {enable = false}
+        }
+      },
+      capabilities = capabilities
+    }
+  else
+    require'lspconfig'[server].setup {capabilities = capabilities}
+  end
+end
