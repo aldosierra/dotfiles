5aeacf655f8a7ff50958f37250c126e068d4a806
Aldo Sierra <aldo.sierrav@gmail.com>
2021-12-15 00:15:02 -0600

    Fix sound script

--- suckless/slstatus/sound.sh
+++ suckless/slstatus/sound.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 snd="$(pamixer --get-volume)"
-if [$snd -eq 0]
+if [ $snd -eq 0 ];
 then
   echo -e "婢 mute"
-elif [$snd -gt 0 && $snd -lt 25]
+elif [ $snd -gt 0 -a $snd -lt 25 ];
 then 
   echo -e "奄 $snd%"
-elif [$snd -gt 24 && -lt 51;]
+elif [ $snd -ge 25 -a $snd -le 50 ];
 then
   echo -e "奔 $snd%"
 else
