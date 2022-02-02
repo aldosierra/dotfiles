4253c31b4058302c1e792509c122e065609c1f55
Aldo Sierra <aldo.sierrav@gmail.com>
2022-02-02 13:35:48 -0600

    Fix some errors

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -14,6 +14,8 @@ return require('packer').startup(function(use)
   use {'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'}}
   -- Whichkey
   use 'folke/which-key.nvim'
+  -- Autopairs
+  use 'windwp/nvim-autopairs'
   -- Telescope
   use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
   -- LSP
